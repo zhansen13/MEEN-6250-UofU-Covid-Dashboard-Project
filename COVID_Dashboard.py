bdd3d812b7fce9e0cf13f7fc853d47de12140d86
collindgrant <77707519+collindgrant@users.noreply.github.com>
2022-12-07 11:50:01 -0700

    Adding HoverTool tooltips

--- COVID_Dashboard.py
+++ COVID_Dashboard.py
@@ -1,23 +1,34 @@
-from bokeh.plotting import figure, output_file, show, ColumnDataSource
+# Necessary imports
+from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
+from bokeh.models.tools import HoverTool
 from ScrapeWebsite import scrape_country
 import json
 
+
+
+# Open the .json file
 f = open('today.json')
 data = json.load(f)
 
+# Specify output .html file
 output_file('Dashboard.html')
 
+# Collect Data from .json file
 countries = list()
+totalDeathsStr = list()
 totalDeaths = list()
 newDeaths = list()
 for i in range(len(data)-1):
+    # If the data field is blank, skip that country
     if data[i]['TotalDeaths'] == '':
         pass
     else:
         countries.append(data[i]['Country,Other'])
+        totalDeathsStr.append(data[i]['TotalDeaths'].replace(',',''))
         totalDeaths.append(int(data[i]['TotalDeaths'].replace(',','')))
         # newDeaths.append(data[i]['NewDeaths'].replace('+','').replace(',',''))
 
+# Set figure specifications
 totalDeathsPerCountry = figure(
     y_range = countries,
     width = 1500,
@@ -36,6 +47,9 @@ totalDeathsPerCountry.hbar(
     color = 'blue',
     fill_alpha = 0.5
 )
+
+# Add Tooltips (HoverTool)
+totalDeathsPerCountry.add_tools(HoverTool(tooltips = [("Country", "@y"),("Total Deaths", "@right")]))
 
 # Show Results
 show(totalDeathsPerCountry)
