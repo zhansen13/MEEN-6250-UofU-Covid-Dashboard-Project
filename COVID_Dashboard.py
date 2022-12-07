09fa5f2379456b2ab1b106c5f2c04c6cee0bde8c
collindgrant <77707519+collindgrant@users.noreply.github.com>
2022-12-07 10:37:52 -0700

    Update COVID_Dashboard.py
    Removing unnecessary lines.

--- COVID_Dashboard.py
+++ COVID_Dashboard.py
@@ -40,21 +40,3 @@ totalDeathsPerCountry.hbar(
 # Show Results
 show(totalDeathsPerCountry)
 
-
-# x = [1,2,3,4,5]
-# y = [4,6,2,4,3]
-
-# output_file('index.html')
-
-# # Add Plot
-# p = figure(
-#     title = 'Simple Example',
-#     x_axis_label = 'X Axis',
-#     y_axis_label = 'Y Axis'
-# )
-
-# # Render glyph
-# p.line(x,y,line_width = 2)
-
-# # Show results
-# show(p)
